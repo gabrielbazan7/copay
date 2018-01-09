19e40ecc9298451e6671dc9501625cc0f43512f3
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-01-09 16:16:29 -0300

    Fix UI for mobile

--- src/providers/app/app.ts
+++ src/providers/app/app.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Logger } from '@nsalaun/ng-logger';
 
@@ -6,6 +7,7 @@ import { LanguageProvider } from '../../providers/language/language';
 import { ConfigProvider } from '../../providers/config/config';
 import { TouchIdProvider } from '../../providers/touchid/touchid';
 import { PersistenceProvider } from '../../providers/persistence/persistence';
+import { PlatformProvider } from '../../providers/platform/platform';
 
 /* TODO: implement interface propertly
 interface App {
@@ -50,10 +52,12 @@ export class AppProvider {
   constructor(
     public http: HttpClient,
     private logger: Logger,
+    private location: Location,
     private language: LanguageProvider,
     private config: ConfigProvider,
     private touchid: TouchIdProvider,
-    private persistence: PersistenceProvider
+    private persistence: PersistenceProvider,
+    private platform: PlatformProvider
   ) {
     this.logger.info('AppProvider initialized.');
   }
